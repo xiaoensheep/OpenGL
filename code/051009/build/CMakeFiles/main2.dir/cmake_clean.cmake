bf7c2a8047e1348fc9e2b1591b1352c7155bc904
Shaunsuns <xiaoe821@gmail.com>
2025-10-10 19:53:28 +0800

    202510101952

---
+++ code/051009/build/CMakeFiles/main2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main2.dir/home/zxy/Development/OpenGL/glad/src/glad.c.o"
+  "CMakeFiles/main2.dir/home/zxy/Development/OpenGL/glad/src/glad.c.o.d"
+  "CMakeFiles/main2.dir/main2.cpp.o"
+  "CMakeFiles/main2.dir/main2.cpp.o.d"
+  "main2"
+  "main2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
