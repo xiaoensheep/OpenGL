53d673f8c69317e0ba18d436d85a7deac6a8bfba
Shaunsuns <xiaoe821@gmail.com>
2025-10-16 13:54:27 +0800

    shader

---
+++ code/build/CMakeFiles/learnshader.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learnshader.dir/2_shader/LearnShader.cpp.o"
+  "CMakeFiles/learnshader.dir/2_shader/LearnShader.cpp.o.d"
+  "CMakeFiles/learnshader.dir/home/zxy/Development/OpenGL/glad/src/glad.c.o"
+  "CMakeFiles/learnshader.dir/home/zxy/Development/OpenGL/glad/src/glad.c.o.d"
+  "learnshader"
+  "learnshader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/learnshader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/build/CMakeFiles/learnshader.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for learnshader.
+# This may be replaced when dependencies are built.
