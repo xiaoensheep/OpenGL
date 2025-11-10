8d1b5a420926e5607e9c81a56cdb0feace481c4f
Shaunsuns <xiaoe821@gmail.com>
2025-11-10 13:07:11 +0800

    LearnCamera

---
+++ code/6_Camera/build/CMakeFiles/LearnCamera.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnCamera.dir/LearnCamera.cpp.o"
+  "CMakeFiles/LearnCamera.dir/LearnCamera.cpp.o.d"
+  "CMakeFiles/LearnCamera.dir/home/zxy/Development/OpenGL/glad/src/glad.c.o"
+  "CMakeFiles/LearnCamera.dir/home/zxy/Development/OpenGL/glad/src/glad.c.o.d"
+  "LearnCamera"
+  "LearnCamera.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/LearnCamera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
