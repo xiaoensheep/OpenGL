23915bce2a3d4960f285a2589225a650a5f356dd
Shaunsuns <xiaoe821@gmail.com>
2025-11-11 12:15:54 +0800

    LearnColors

---
+++ code/7_Colors/build/CMakeFiles/LearnColors.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnColors.dir/LearnColors.cpp.o"
+  "CMakeFiles/LearnColors.dir/LearnColors.cpp.o.d"
+  "CMakeFiles/LearnColors.dir/home/zxy/Development/OpenGL/glad/src/glad.c.o"
+  "CMakeFiles/LearnColors.dir/home/zxy/Development/OpenGL/glad/src/glad.c.o.d"
+  "LearnColors"
+  "LearnColors.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/LearnColors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/7_Colors/build/CMakeFiles/LearnColors.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LearnColors.
+# This may be replaced when dependencies are built.
