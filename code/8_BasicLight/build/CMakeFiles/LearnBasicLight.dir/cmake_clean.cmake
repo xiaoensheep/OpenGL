e2f2608e0df397136d5cc4e14517553c7665db45
Shaunsuns <xiaoe821@gmail.com>
2025-11-13 16:29:53 +0800

    LearnBasicLight

---
+++ code/8_BasicLight/build/CMakeFiles/LearnBasicLight.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnBasicLight.dir/LearnBasicLight.cpp.o"
+  "CMakeFiles/LearnBasicLight.dir/LearnBasicLight.cpp.o.d"
+  "CMakeFiles/LearnBasicLight.dir/home/zxy/Development/OpenGL/glad/src/glad.c.o"
+  "CMakeFiles/LearnBasicLight.dir/home/zxy/Development/OpenGL/glad/src/glad.c.o.d"
+  "LearnBasicLight"
+  "LearnBasicLight.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/LearnBasicLight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/8_BasicLight/build/CMakeFiles/LearnBasicLight.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LearnBasicLight.
+# This may be replaced when dependencies are built.
